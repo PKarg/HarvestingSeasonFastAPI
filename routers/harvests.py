93f900e9ac4438242c0477cc99e5b0d3c4662915
PKarg <patryk.kargulewicz@gmail.com>
2022-04-26 22:32:14 +0200

    added filters to endpoint /harvests

--- routers/harvests.py
+++ routers/harvests.py
@@ -1,6 +1,6 @@
-from typing import List
+from typing import List, Optional
 
-from fastapi import APIRouter, Depends, HTTPException, status
+from fastapi import APIRouter, Depends, HTTPException, status, Query
 from sqlalchemy.orm import Session
 
 from auth import get_current_user
@@ -17,8 +17,17 @@ router = APIRouter(
 @router.get("/", status_code=status.HTTP_200_OK,
             response_model=List[sc.HarvestResponse])
 def harvests_get_all(user: m.User = Depends(get_current_user),
-                     db: Session = Depends(get_db)):
-    harvests_m_all: List[m.Harvest] = crud.harvest_get(db, user)
+                     db: Session = Depends(get_db),
+                     after: Optional[str] = Query(None),
+                     before: Optional[str] = Query(None),
+                     fruit: Optional[str] = Query(None, min_length=5, max_length=20),
+                     p_more: Optional[str] = Query(None, regex=r"^ *\d[\d ]*$"),
+                     p_less: Optional[str] = Query(None, regex=r"^ *\d[\d ]*$"),
+                     h_more: Optional[str] = Query(None, regex=r"^ *\d[\d ]*$"),
+                     h_less: Optional[str] = Query(None, regex=r"^ *\d[\d ]*$")
+                     ):
+    harvests_m_all: List[m.Harvest] = crud.harvest_get(db, user, after=after, before=before, fruit=fruit,
+                                                       p_more=p_more, p_less=p_less, h_more=h_more, h_less=h_less)
     return harvests_m_all
 
 
@@ -30,5 +39,5 @@ def harvests_get_all(id: int,
     try:
         harvests_m: m.Harvest = crud.harvest_get(db, user, id=id)[0]
         return harvests_m
-    except IndexError as e:
+    except IndexError:
         raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail="Object with given id doesn't exist")
